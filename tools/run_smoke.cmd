a900840c071e54a83a94c2109af8ab6c4e9c9f3d
Maxim Samokhval <msamohval.tqm@gmail.com>
2021-11-30 16:08:18 +0200

    fix: smoke.json

--- tools/run_smoke.cmd
+++ tools/run_smoke.cmd
@@ -1,3 +1,6 @@
 @chcp 65001
 rem set LOGOS_CONFIG=logger.rootLogger=DEBUG
+rem удаление результатов предыдущих тестов
+@echo off
+del /f /s /q "./out/smoke"
 @call vrunner xunit --settings ./tools/json/vrunner.json
